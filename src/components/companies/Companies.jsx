9dfd1888e307ab3683245a8b2497eda6b1c2b142
Reyes Rondon <reyes192119@gmail.com>
2024-08-05 14:39:51 -0400

    slider fixed

--- src/components/companies/Companies.jsx
+++ src/components/companies/Companies.jsx
@@ -1,43 +1,16 @@
-import {Carousel} from "primereact/carousel"
-import {useCustomFunctions} from "../../hooks"
 import {languages} from "../../helpers/languages"
 
+// Import Swiper React components
+import {Swiper, SwiperSlide} from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+import {Navigation, Pagination, Mousewheel, Keyboard} from "swiper/modules"
+
 export const Companies = ({companies}) => {
-  const {responsiveOptions} = useCustomFunctions()
-
-  const personTemplate = (company) => {
-    return (
-      <div className="flex justify-center p-0 m-0">
-        <div className="flex flex-col justify-center items-center">
-          <img
-            src={
-              company.logo_path == null
-                ? "/movie-play.svg"
-                : `https://media.themoviedb.org/t/p/w780/${company.logo_path}`
-            }
-            alt={`${company.name}`}
-            className={`w-16 h-16 md:w-32 md:h-32 object-contain ${
-              company.logo_path == null ? "invert" : ""
-            }`}
-          />
-
-          <div className="mt-2">
-            <p className="text-[10px] sm:text-xs md:text-sm font-bold text-center">
-              {company.name}
-            </p>
-            <span className="text-[10px] sm:text-xs md:text-sm text-black text-center">
-              {
-                languages.filter(
-                  (lang) => lang.iso_3166_1 === company.origin_country
-                )[0]?.english_name
-              }
-            </span>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div
       className={` my-5 items-center justify-center bg-[slate-900] h-auto ${
@@ -52,14 +25,65 @@ export const Companies = ({companies}) => {
         </div>
         <div className="bg-[#3B82F6] bg-opacity-80 rounded-2xl py-3">
           <div>
-            <Carousel
-              value={companies}
-              numVisible={8}
-              numScroll={3}
-              responsiveOptions={responsiveOptions()}
-              itemTemplate={personTemplate}
-              circular
-            />
+            <Swiper
+              loop={true}
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 5,
+                  spaceBetween: 30,
+                },
+              }}
+              cssMode={true}
+              navigation={true}
+              mousewheel={true}
+              keyboard={true}
+              modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+              className="mySwiper"
+            >
+              {companies.map((company) => {
+                return (
+                  <SwiperSlide key={company.id}>
+                    <div className="flex justify-center p-0 m-0">
+                      <div className="flex flex-col justify-center items-center">
+                        <img
+                          src={
+                            company.logo_path == null
+                              ? "/movie-play.svg"
+                              : `https://media.themoviedb.org/t/p/w780/${company.logo_path}`
+                          }
+                          alt={`${company.name}`}
+                          className={`w-16 h-16 md:w-32 md:h-32 object-contain ${
+                            company.logo_path == null ? "invert" : ""
+                          }`}
+                        />
+
+                        <div className="mt-2">
+                          <p className="text-[10px] sm:text-xs md:text-sm font-bold text-center">
+                            {company.name}
+                          </p>
+                          <span className="text-[10px] sm:text-xs md:text-sm text-black text-center">
+                            {
+                              languages.filter(
+                                (lang) =>
+                                  lang.iso_3166_1 === company.origin_country
+                              )[0]?.english_name
+                            }
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                )
+              })}
+            </Swiper>
           </div>
         </div>
       </div>
