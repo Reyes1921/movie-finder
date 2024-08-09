406e6a34733a6c31119040e7b39d9c9528151045
Reyes Rondon <reyes192119@gmail.com>
2024-08-09 13:34:24 -0400

    more

--- src/components/movie/MovieGrid.jsx
+++ src/components/movie/MovieGrid.jsx
@@ -4,7 +4,7 @@ export const MovieGrid = ({movieData}) => {
   return (
     <>
       <h1 className="hidden">Movie Finder</h1>
-      <div className="grid gap-2 grid-cols-2 md:grid-cols-2 lg:grid-cols-5 pt-0 p-">
+      <div className="grid gap-2 grid-cols-2 md:grid-cols-2 lg:grid-cols-5 pt-0 p-3">
         {movieData.map((movie) => {
           return (
             <ItemGridMovies movie={movie} key={movie.id + Math.random(0, 1)} />
