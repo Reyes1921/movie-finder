e7a487ad0a49be24e66a0c7814a8e76623a1e01d
Reyes Rondon <reyes192119@gmail.com>
2024-08-08 21:05:04 -0400

    last chanegs in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -75,13 +75,13 @@ export function Header() {
                 containerProps={{
                   className: "min-w-[288px]",
                 }}
-                className=" !border-t-blue-gray-300 pl-9 placeholder:text-blue-gray-300 focus:!border-[#2074F6] rounded-xl"
+                className=" !border-t-blue-gray-300 pl-9 placeholder:text-blue-gray-300 focus:!border-[#2074F6] rounded-xl min-h-10"
                 labelProps={{
                   className: "before:content-none after:content-none",
                 }}
               />
             </form>
-            <div className="!absolute left-3 top-[13px]">
+            <div className="absolute left-3 top-[13px]">
               <svg
                 width="13"
                 height="14"
